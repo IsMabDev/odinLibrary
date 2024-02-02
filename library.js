4380db30845fe80a0059f04d9b3a60f5c133a172
mabroukissam <145654259+MabroukIssam@users.noreply.github.com>
2024-02-02 16:38:58 +0100

    Improvement of the UI and first commit for the js

--- library.js
+++ library.js
@@ -1,4 +1,5 @@
 const myLibrary = [];
+isEmpty = true;
 
 const booksContainer = document.querySelector("#booksContainer");
 
@@ -17,12 +18,52 @@ function addBookToLibrary(object) {
   myLibrary.push(object);
 }
 const theHobbit = new Book("The Hobbit", "J.R.R Toliken", 295, true);
-addBookToLibrary(theHobbit);
-myLibrary;
-if (myLibrary.length === 0) {
-  booksContainer.innerHTML = `<p>For the moment your library is empty.</p>
-      <p>Your can add a new book using the button below</p>`;
-}
+//addBookToLibrary(theHobbit);
 
-z = theHobbit.info();
-z;
+const addBookButton = document.querySelector("#addBook");
+
+const addBookDialog = document.querySelector("#bookDialog");
+
+const cancelDialog = document.querySelector("#cancelDialog");
+
+const saveDialog = document.querySelector("#saveDialog");
+
+const titleNewBook = document.querySelector("#title");
+updateLibrary();
+addBookButton.addEventListener("click", () => {
+  addBookDialog.showModal();
+});
+
+cancelDialog.addEventListener("click", () => {
+  addBookDialog.close();
+});
+
+saveDialog.addEventListener("click", (e) => {
+  e.preventDefault();
+  const newBook = new Book(titleNewBook.value, "qsdf", 321, true);
+  addBookToLibrary(newBook);
+  myLibrary;
+  console.log("myLibrary: ", myLibrary);
+  updateLibrary();
+  addBookDialog.close();
+});
+
+function updateLibrary() {
+  if (myLibrary.length === 0 && isEmpty) {
+    booksContainer.innerHTML = `<div id="emptyLibrary"><p>For the moment your library is empty.</p>
+      <p>Your can add a new book using the button above</p></div>`;
+  } else {
+    if (isEmpty) {
+      booksContainer.innerHTML = "";
+    }
+    booksContainer.innerHTML += `<div class="bookCard">
+        <h1 class="bookTitle">Title: ${
+          myLibrary[myLibrary.length - 1].title
+        }</h1>
+        <h4 class="bookAuthor">Author: My author</h4>
+        <h4 class="bookPagesNumber">Pages Number: 295</h4>
+        <button class="isRead" title="isRead" value="true"></button>
+      </div>`;
+    isEmpty = false;
+  }
+}
